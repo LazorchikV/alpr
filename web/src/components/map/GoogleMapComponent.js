11a89b25c3d91eb79fe55a22d6b8a5ffea1c8108
LazorchikV <lazorchik29081989@gmail.com>
2025-02-09 20:46:16 +0200

    Added google maps api key.

--- web/src/components/map/GoogleMapComponent.js
+++ web/src/components/map/GoogleMapComponent.js
@@ -2,7 +2,7 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
-  height: "400px",
+  height: "800px",
 };
 
 const center = {
@@ -10,9 +10,11 @@ const center = {
   lng: -75.6972,
 };
 
+const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
 const GoogleMapComponent = () => {
   return (
-    <LoadScript googleMapsApiKey="GOOGLE_MAPS_API_KEY">
+    <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
         <Marker position={center} />
       </GoogleMap>
